9216bae6eb93714fb29e5b01170b61a8aef6a98e
Nick Kocharhook <nick@kocharhook.com>
2019-04-25 12:50:38 +0100

    Increase size of session_id column to avoid Postgres commit errors.

--- flask_sessionstore/sessions.py
+++ flask_sessionstore/sessions.py
@@ -484,7 +484,7 @@ class SqlAlchemySessionInterface(SessionInterface):
             __tablename__ = table
 
             id = self.db.Column(self.db.Integer, primary_key=True)
-            session_id = self.db.Column(self.db.String(255), unique=True)
+            session_id = self.db.Column(self.db.String(2047), unique=True)
             data = self.db.Column(self.db.Text)
             expiry = self.db.Column(self.db.DateTime)
 
